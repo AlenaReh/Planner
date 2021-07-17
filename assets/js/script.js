8b10304e0ca99493e22eac0c1f1bb57ac39f53dc
Alena Rehberger <alenarehberger92@gmail.com>
2021-07-17 12:13:05 +0500

    Added present and future styles but it doesn't work right yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -107,4 +107,109 @@ if (currentTime > ninePm) {
   $("#event-9pm").removeClass("present future").addClass("past");
 }
 
+// Color Coding for present events
+if (currentTime = fiveAm) {
+  $("#event-5am").removeClass("past future").addClass("present");
+}
+if (currentTime = sixAm) {
+  $("#event-6am").removeClass("past future").addClass("present");
+}
+if (currentTime = sevenAm) {
+  $("#event-7am").removeClass("past future").addClass("present");
+}
+if (currentTime = eightAm) {
+  $("#event-8am").removeClass("past future").addClass("present");
+}
+if (currentTime = nineAm) {
+  $("#event-9am").removeClass("past future").addClass("present");
+}
+if (currentTime = tenAm) {
+  $("#event-10am").removeClass("past future").addClass("present");
+}
+if (currentTime = elevenAm) {
+  $("#event-11am").removeClass("past future").addClass("present");
+}
+if (currentTime = twelveAm) {
+  $("#event-12pm").removeClass("past future").addClass("present");
+}
+if (currentTime = onePm) {
+  $("#event-1pm").removeClass("past future").addClass("present");
+}
+if (currentTime = twoPm) {
+  $("#event-2pm").removeClass("past future").addClass("present");
+}
+if (currentTime = threePm) {
+  $("#event-3pm").removeClass("past future").addClass("present");
+}
+if (currentTime = fourPm) {
+  $("#event-4pm").removeClass("past future").addClass("present");
+}
+if (currentTime = fivePm) {
+  $("#event-5pm").removeClass("past future").addClass("present");
+}
+if (currentTime = sixPm) {
+  $("#event-6pm").removeClass("past future").addClass("present");
+}
+if (currentTime = sevenPm) {
+  $("#event-7pm").removeClass("past future").addClass("present");
+}
+if (currentTime = eightPm) {
+  $("#event-8pm").removeClass("past future").addClass("present");
+}
+if (currentTime = ninePm) {
+  $("#event-9pm").removeClass("past future").addClass("present");
+}
 
+
+// Color Coding for future events
+if (currentTime < fiveAm) {
+  $("#event-5am").removeClass("present past").addClass("future");
+}
+if (currentTime < sixAm) {
+  $("#event-6am").removeClass("present past").addClass("future");
+}
+if (currentTime < sevenAm) {
+  $("#event-7am").removeClass("present past").addClass("future");
+}
+if (currentTime < eightAm) {
+  $("#event-8am").removeClass("present past").addClass("future");
+}
+if (currentTime < nineAm) {
+  $("#event-9am").removeClass("present past").addClass("future");
+}
+if (currentTime < tenAm) {
+  $("#event-10am").removeClass("present past").addClass("future");
+}
+if (currentTime < elevenAm) {
+  $("#event-11am").removeClass("present past").addClass("future");
+}
+if (currentTime < twelveAm) {
+  $("#event-12pm").removeClass("present past").addClass("future");
+}
+if (currentTime < onePm) {
+  $("#event-1pm").removeClass("present past").addClass("future");
+}
+if (currentTime < twoPm) {
+  $("#event-2pm").removeClass("present past").addClass("future");
+}
+if (currentTime < threePm) {
+  $("#event-3pm").removeClass("present past").addClass("future");
+}
+if (currentTime < fourPm) {
+  $("#event-4pm").removeClass("present past").addClass("future");
+}
+if (currentTime < fivePm) {
+  $("#event-5pm").removeClass("present past").addClass("future");
+}
+if (currentTime < sixPm) {
+  $("#event-6pm").removeClass("present past").addClass("future");
+}
+if (currentTime < sevenPm) {
+  $("#event-7pm").removeClass("present past").addClass("future");
+}
+if (currentTime < eightPm) {
+  $("#event-8pm").removeClass("present past").addClass("future");
+}
+if (currentTime < ninePm) {
+  $("#event-9pm").removeClass("present past").addClass("future");
+}
